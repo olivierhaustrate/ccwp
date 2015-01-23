c020a748eff8dfdd51b0a2a6c3e47902398c10ab
olivier <olivier.haustrate@hotmail.com>
2015-01-23 22:47:38 +0100

    init repo

---
+++ config/local-config.php
@@ -0,0 +1,12 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+
+You may include other settings here that you only want enabled on your local development checkouts
+*/
+
+define( 'DB_NAME', 'db_name' );
+define( 'DB_USER', 'bd_user' );
+define( 'DB_PASSWORD', 'db_password' );
+define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// WordPress view bootstrapper
+define( 'WP_USE_THEMES', true );
+require( './web/core/wp-blog-header.php' );
